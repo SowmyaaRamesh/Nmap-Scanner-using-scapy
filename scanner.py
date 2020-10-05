dc50ae15903a4e18309406a8f8b37e67865b828a
Sowmya <63141508+SowmyaaRamesh@users.noreply.github.com>
2020-10-05 09:09:30 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -53,35 +53,3 @@ print("Not shown: "+ str(closed_ports) +" closed ports")
 
 
 
-'''
-OUTPUT:
-
-
-Starting Nmap 7.80 (https://nmap.org) at 2020-10-02 21:42 IST
-Nmap scan report for scanme.nmap.org 45.33.32.156
-Host is up
-
-PORT	STATE
-22      Open
-25      Filtered
-80      Open
-206     Filtered
-219     Filtered
-266     Filtered
-347     Filtered
-349     Filtered
-360     Filtered
-384     Filtered
-397     Filtered
-416     Filtered
-483     Filtered
-538     Filtered
-551     Filtered
-665     Filtered
-684     Filtered
-721     Filtered
-729     Filtered
-730     Filtered
-Not shown: 779 closed ports
-
-'''
